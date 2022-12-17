bdf6742d906f5e560fb70b73b5ea50f2d4d89d97
Inoue Shintaro <shintaro-inoue-mac@g.ecc.u-tokyo.ac.jp>
2022-12-17 23:49:54 +0900

    add: sensor publisher

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,12 @@
 #include <Wire.h>
-
 #include <Adafruit_PWMServoDriver.h>
-
-#include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
 #include <ros.h>
-#include <std_msgs/Float32MultiArray.h>
-// #include <std_msgs/Int32MultiArray.h>
-//#include <std_msgs/Int16MultiArray.h>
+#include <std_msgs/Int16MultiArray.h>
+#include <std_msgs/UInt8MultiArray.h>
+#include <sensor_msgs/Imu.h>
 
 ros::NodeHandle nh;
 
@@ -18,35 +15,29 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 const uint8_t PINS[6] = {0, 1, 2, 3, 4, 5};
 const uint8_t MINS[6] = {122, 110, 115, 102, 102, 102}; // 102, 110
 const int MAXS[6] = {490, 487, 485, 491, 491, 491}; // 491, 480
-int angles[6] = {90, 90, 90, 90, 90, 90};
-
-void servoSetup()
-{ 
-  pwm.begin();         // 初期設定
-  pwm.setPWMFreq(50);  // PWM周期を50Hzに設定
-}
+uint16_t angles[6] = {90, 90, 90, 90, 90, 90};
 
 void setAngle(int id, int angle)
 {
   pwm.setPWM(PINS[id], 0, map(angle, 0, 180, MINS[id], MAXS[id]));
 }
 
-void servoCb(const std_msgs::Float32MultiArray& msg)
+void servoCb(const std_msgs::Int16MultiArray& msg)
 {
-  //angles = msg;
   for (int i = 0; i < 6; ++i)
   {
     angles[i] = msg.data[i];
   }
-
-  if (msg.data[2] == 0) {
-    digitalWrite(13, HIGH);
-  } else {
-    digitalWrite(13, LOW);
-  }
 }
 
-ros::Subscriber<std_msgs::Float32MultiArray> servo_sub("servo/command", servoCb);
+ros::Subscriber<std_msgs::Int16MultiArray> servo_sub("servo/command", servoCb);
+
+void servoSetup()
+{
+  pwm.begin();
+  pwm.setPWMFreq(50);
+  nh.subscribe(servo_sub);
+}
 
 void servoLoop()
 {
@@ -56,21 +47,75 @@ void servoLoop()
   }
 }
 
+// Sensor
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire);
+sensor_msgs::Imu imu_msg;
+std_msgs::UInt8MultiArray calib_msg;
+
+ros::Publisher imu_pub("sensor/imu", &imu_msg);
+ros::Publisher calib_pub("sensor/calib", &calib_msg);
+
+void sensorSetup()
+{
+  bno.begin();
+  bno.setExtCrystalUse(true);
+
+  nh.advertise(imu_pub);
+  nh.advertise(calib_pub);
+
+  calib_msg.data = (uint8_t*)malloc(sizeof(int8_t) * 4);
+  calib_msg.data_length = 4;
+}
+
+void sensorLoop()
+{
+  imu::Quaternion quat = bno.getQuat();
+  imu_msg.orientation.x = quat.x();
+  imu_msg.orientation.y = quat.y();
+  imu_msg.orientation.z = quat.z();
+  imu_msg.orientation.w = quat.w();
+
+  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu_msg.linear_acceleration.x = acc.x();
+  imu_msg.linear_acceleration.y = acc.y();
+  imu_msg.linear_acceleration.z = acc.z();
+  
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  calib_msg.data[0] = system;
+  calib_msg.data[1] = gyro;
+  calib_msg.data[2] = accel;
+  calib_msg.data[3] = mag;
+
+  imu_pub.publish(&imu_msg);
+  calib_pub.publish(&calib_msg);
+}
+
+// main
+unsigned long servo_timer = 0;
+unsigned long sensor_timer = 0;
+
 void setup() {
   nh.initNode();
+
   servoSetup();
+  sensorSetup();
 
-  pinMode(13, OUTPUT);
-
-  nh.subscribe(servo_sub);
-  
   delay(1000);
 }
 
 void loop() {
+  unsigned long now = millis();
+
+  if ((now - servo_timer) > 50 ) {
+    servoLoop();
+    servo_timer = now;
+  }
+
+  if ((now - sensor_timer) > 100 ) {
+    sensorLoop();
+    sensor_timer = now;
+  }
+  
   nh.spinOnce();
-
-  servoLoop();
-  delay(100);
-
 }
--- src/control.py
+++ src/control.py
@@ -3,29 +3,19 @@
 import rospy
 import std_msgs.msg
 import time
-import sys
 
-# ノードの初期化
 rospy.init_node('control', anonymous=True)
 
-# Float32MultiArray型のメッセージを定義
-data = std_msgs.msg.Float32MultiArray()
-# see https://answers.ros.org/question/380890/unable-to-publish-float64multiarray-in-python/
-data.layout.dim = [std_msgs.msg.MultiArrayDimension(label='dim0', size=6, stride=6*4)]
-# data.layout.append(std_msgs.msg.MultiArrayLayout())
-# data.layout.dim[0].size = 6
+data = std_msgs.msg.Int16MultiArray()
 
-# パブリッシュするトピックを定義
-pub = rospy.Publisher('servo/command', std_msgs.msg.Float32MultiArray, queue_size=1)
+pub = rospy.Publisher('servo/command', std_msgs.msg.Int16MultiArray, queue_size=1)
 
 step = 0
 st = time.time()
-# トピックにメッセージをパブリッシュ
-rate = rospy.Rate(1)
+rate = rospy.Rate(5)
 while not rospy.is_shutdown():
   if step % 3 == 0:
     data.data = [0, 0, 0, 0, 0, 0]
-    # data.data = [60, 60, 60, 60, 60, 60]
   elif step % 3 == 1:
     data.data = [90, 90, 90, 90, 90, 90]
   else:
@@ -33,10 +23,8 @@ while not rospy.is_shutdown():
 
   pub.publish(data)
 
-  # print(sys.getsizeof(data)) 72
   rate.sleep()
 
-  if (time.time() - st) >= 2:
+  if (time.time() - st) >= 1:
     step += 1
     st = time.time()
-
