ed089b33ff59db9561e59f53fea9f67dc2459124
leus <s-inoue@jsk.imi.i.u-tokyo.ac.jp>
2023-06-21 08:47:35 +0900

    fix: 引っ越し

--- src/nn.py
+++ src/nn.py
@@ -10,6 +10,7 @@ import rospy
 from std_msgs.msg import Int16MultiArray
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Vector3
+from utils import *
 
 from config import *
 
@@ -54,14 +55,15 @@ def run_onnx_model(model_path, input_data):
   return outputs[0]
 
 def simRad2realDeg(sim_rad):
+    print(sim_rad)
     deg = -np.rad2deg(sim_rad) + 90
     real_deg = np.zeros([1, 6])
-    real_deg[0, 0] = deg[0, 4]
-    real_deg[0, 1] = deg[0, 5]
-    real_deg[0, 2] = deg[0, 2]
-    real_deg[0, 3] = deg[0, 3]
-    real_deg[0, 4] = deg[0, 0]
-    real_deg[0, 5] = deg[0, 1]
+    real_deg[0, 0] = deg[0, 5]
+    real_deg[0, 1] = deg[0, 4]
+    real_deg[0, 2] = deg[0, 3]
+    real_deg[0, 3] = deg[0, 2]
+    real_deg[0, 4] = deg[0, 1]
+    real_deg[0, 5] = deg[0, 0]
     return real_deg
 
 if __name__=="__main__":
@@ -70,12 +72,17 @@ if __name__=="__main__":
   rospy.Subscriber("sensor/imu", Imu, imuCb)
   pub = rospy.Publisher('servo/command', Int16MultiArray, queue_size=1)
   rate = rospy.Rate(20)
-  model_path=(os.environ['HOME'] + "/IsaacGymEnvs/isaacgymenvs/runs/Chair_09-15-53-41/nn/Chair.onnx")
+  model_path=(os.environ['HOME'] + "/IsaacGymEnvs/isaacgymenvs/runs/Chair_20-19-55-15/nn/Chair.onnx")
   numRotationHis = 7
   numActionHis = 7
   rotation_history = np.zeros([numRotationHis, 4])
   rotation_history[:,  3] = 1.0
   action_history = np.ones([numActionHis, 6])
+  
+  # device='cuda'
+  # start_rotation = torch.tensor([0, 0, 0, 1], device=device)
+  # inv_start_rot = quat_conjugate(start_rotation)
+  # targets = to_torch([10, 0, 0], device=.device)
   
   while not rospy.is_shutdown():
     # action
@@ -91,5 +98,12 @@ if __name__=="__main__":
     rotation = np.array([[-imu_msg.orientation.x, -imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w]])
     rotation_history = np.concatenate([rotation, rotation_history], 0)[:-1, :]
     action_history = np.concatenate([action, action_history], 0)[:-1, :]
+
+    # torso_rotation = torch.tensor(rotation, device=device)
+    # to_target = targets - torso_position
+    # to_target[:, 2] = 0
+    # torso_quat, up_proj, heading_proj, up_vec, heading_vec = compute_heading_and_up(
+    #     torso_rotation, inv_start_rot, to_target, basis_vec0, basis_vec1, 2)
+    # roll, pitch, yaw, angle_to_target = compute_rot(torso_quat, targets, torso_position)
 
     rate.sleep()
